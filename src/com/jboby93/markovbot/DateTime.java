253a5c80539efd963efa69cfa10cc53108a0ae11
Mungrel <npol807@aucklanduni.ac.nz>
2017-04-03 20:30:36 +1200

    Heavily simplified DateTime class and date related methods, also uses objects instead of static methods now

--- src/com/jboby93/markovbot/DateTime.java
+++ src/com/jboby93/markovbot/DateTime.java
@@ -1,58 +1,37 @@
 package com.jboby93.markovbot;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 public class DateTime {
 	
-	private Calendar calendar;
+	private Date date;
 	private SimpleDateFormat longDate;
 	private SimpleDateFormat timeStamp;
 	
 	public DateTime(){
-		this.calendar = Calendar.getInstance();
+		this.date = new Date();
 		this.longDate = new SimpleDateFormat("MMMM DD, YYYY hh:mm a 'Z'");
 		this.timeStamp = new SimpleDateFormat("HH:mm:ss");
 	}
 	
 	
-
-	public static String getDateString() {
-		Calendar c = Calendar.getInstance();
-	
-		String[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September",
-				"October", "November", "December" };
-	
-		return months[c.get(Calendar.MONTH)] + " " + c.get(Calendar.DAY_OF_MONTH) + ", " + c.get(Calendar.YEAR) + " "
-				+ c.get(Calendar.HOUR) + ":"
-				+ (c.get(Calendar.MINUTE) < 10 ? "0" + c.get(Calendar.MINUTE) : c.get(Calendar.MINUTE)).toString()
-				+ (c.get(Calendar.AM_PM) == Calendar.AM ? "am" : "pm") + " " + TimeZone.getDefault()
-						.getDisplayName(TimeZone.getDefault().inDaylightTime(new Date()), TimeZone.SHORT);
+	// Long date
+	public String getDateString() {
+		return longDate.format(date);
 	}
 
-	public static String getTimeStampForFileName() {
-		Calendar c = Calendar.getInstance();
-	
-		return (c.get(Calendar.HOUR_OF_DAY) < 10 ? "0" + c.get(Calendar.HOUR_OF_DAY) : c.get(Calendar.HOUR_OF_DAY))
-				.toString()
-				+ (c.get(Calendar.MINUTE) < 10 ? "0" + c.get(Calendar.MINUTE) : c.get(Calendar.MINUTE)).toString()
-				+ (c.get(Calendar.SECOND) < 10 ? "0" + c.get(Calendar.SECOND) : c.get(Calendar.SECOND)).toString();
+	// Short date
+	public String getTimeStampForFileName() {
+		return timeStamp.format(date);
 	}
 
-	public static String getTimeStamp() {
-		Calendar c = Calendar.getInstance();
-		return "["
-				+ (c.get(Calendar.HOUR_OF_DAY) < 10 ? "0" + c.get(Calendar.HOUR_OF_DAY) : c.get(Calendar.HOUR_OF_DAY))
-						.toString()
-				+ ":" + (c.get(Calendar.MINUTE) < 10 ? "0" + c.get(Calendar.MINUTE) : c.get(Calendar.MINUTE)).toString()
-				+ ":" + (c.get(Calendar.SECOND) < 10 ? "0" + c.get(Calendar.SECOND) : c.get(Calendar.SECOND)).toString()
-				+ "]";
+	// Short date with []
+	public String getTimeStamp() {
+		return "["+timeStamp.format(date)+"]";
 	}
 
-	public static long getUNIXTimestamp() {
-		return Calendar.getInstance().getTimeInMillis() / 1000;
+	public long getUNIXTimestamp() {
+		return date.getTime() / 1000;
 	}
-	
 }
