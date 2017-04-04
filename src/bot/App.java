1c21f3b067ba95a7906aea9d69e15d786a285af9
Mungrel <npol807@aucklanduni.ac.nz>
2017-04-04 15:24:36 +1200

    Removed erraneous, unused variables in App, processID is never used, but I'll keep the methods/classes used to obtain it

--- src/bot/App.java
+++ src/bot/App.java
@@ -26,13 +26,9 @@ public class App {
 	 * they are used in some component I haven't seen yet If they actually
 	 * aren't being used, you should consider removal
 	 */
-	@SuppressWarnings("unused")
-	private static String processID = null; // Process ID
 	private static MarkovBot bot; // The bot to use
 	private static String lastResult = null; // Last generated result
 	private static String startTimeString = null;
-	@SuppressWarnings("unused")
-	private static long startTime = -1;
 
 	private static Scanner reader; // Scanner over BufferedReader, little I/O and less exception handling
 
@@ -43,12 +39,9 @@ public class App {
 	public static void main(String args[]) throws IOException {
 		DateTime dateTime = new DateTime();
 		startTimeString = dateTime.getDateString();
-		startTime = dateTime.getUNIXTimestamp();
 		reader = new Scanner(System.in);
 
 		about();
-		processID = new ProcessManager().getProcessID();
-
 		try {
 			Logger.openLogFile();
 			Logger.info(name + " - v" + version + " (" + build_date + ")");
